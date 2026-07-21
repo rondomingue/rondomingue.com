a40e2fa4cc3d5ae8904d21186c24485ce4c21327
Ron Domingue <me@rondomingue.com>
2026-07-20 21:23:37 -0500

    Limit homepage to random featured projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,15 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("randomProjects", (projects, count = 6) => {
+    const shuffled = [...projects];
+
+    for (let index = shuffled.length - 1; index > 0; index--) {
+      const randomIndex = Math.floor(Math.random() * (index + 1));
+      [shuffled[index], shuffled[randomIndex]] = [shuffled[randomIndex], shuffled[index]];
+    }
+
+    return shuffled.slice(0, count);
+  });
+
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/fonts");
